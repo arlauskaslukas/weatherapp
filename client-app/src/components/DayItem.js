0732761e11b17ff7c00281dceb4f42c1cbf519e1
Lukas Arlauskas <lukasarlauskas@Lukass-MacBook-Air.local>
2021-05-26 11:46:08 +0300

    frontend almost done

--- client-app/src/components/DayItem.js
+++ client-app/src/components/DayItem.js
@@ -1,79 +1,210 @@
-import { FormControlLabel, makeStyles, Paper, Switch, Typography, Grid } from '@material-ui/core';
-import React, { useState } from 'react';
-import WbSunnyIcon from '@material-ui/icons/WbSunny';
-import WbCloudyIcon from '@material-ui/icons/WbCloudy';
-import OpacityIcon from '@material-ui/icons/Opacity';
-import WhatshotIcon from '@material-ui/icons/Whatshot';
+import {
+  FormControlLabel,
+  makeStyles,
+  Paper,
+  Switch,
+  Typography,
+  Grid,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import WbSunnyIcon from "@material-ui/icons/WbSunny";
+import WbCloudyIcon from "@material-ui/icons/WbCloudy";
+import OpacityIcon from "@material-ui/icons/Opacity";
+import Brightness4OutlinedIcon from "@material-ui/icons/Brightness4Outlined";
+import InvertColorsIcon from "@material-ui/icons/InvertColors";
+import Brightness3Icon from "@material-ui/icons/Brightness3";
 
-const determineIcon = (condition)=>{
-    if(condition==="Sunny"){
-        return <WbSunnyIcon style={{fontSize:'50', color:'#a89200'}}/>;
-    }
-    else if(condition==="Cloudy"){
-        return <WbCloudyIcon style={{fontSize:'50', color:'#7a829e'}}/>;
-    }
-    else if(condition==="Rainy"){
-        return <OpacityIcon style={{fontSize:'50', color:'#85a0ff'}}/>
-    }
-}
+const determineIcon = (condition) => {
+  if (condition === "Sunny") {
+    return <WbSunnyIcon style={{ fontSize: "50", color: "#a89200" }} />;
+  } else if (condition === "Cloudy") {
+    return <WbCloudyIcon style={{ fontSize: "50", color: "#7a829e" }} />;
+  } else if (condition === "Rainy") {
+    return <OpacityIcon style={{ fontSize: "50", color: "#85a0ff" }} />;
+  }
+};
 
-const temp = (temperature, isCelsius)=>{
-    if(isCelsius) {
-        return temperature;
-    }
-    else
-    {
-        return temperature * 1.8 + 32;
-    }
-}
+const BeaufortScale = (windspeed) => {
+  if (windspeed < 0.5) {
+    return "Calm";
+  } else if (windspeed <= 5.5) {
+    return "Gentle breeze";
+  } else if (windspeed < 14.0) {
+    return "Strong Breeze";
+  } else if (windspeed < 24.0) {
+    return "Gale";
+  } else if (windspeed < 32.0) {
+    return "Storm";
+  }
+  return "Hurricane";
+};
+
+const temp = (temperature, isCelsius) => {
+  if (isCelsius) {
+    return temperature;
+  } else {
+    return temperature * 1.8 + 32;
+  }
+};
 
 const useStyles = makeStyles({
-    paperstyle: {
-        backgroundColor: `rgba(255,255,255,0.5)`,
-        minHeight: '50%',
-        width:'50%',
-        padding:'20px',
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'center',
-    }
+  paperstyle: {
+    backgroundColor: `rgba(255,255,255,0.5)`,
+    minHeight: "50%",
+    width: "90%",
+    padding: "20px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
 });
 
 function DayItem(props) {
-    const classes = useStyles();
-    const [isCelsius, setIsCelsius] = useState(true);
-    const [isNightlyCelsius, setIsNightlyCelsius] = useState(true);
-    return (
-        <Paper elevation={2} className={classes.paperstyle}>
-            <Typography variant="h5" style={{fontWeight:'bold'}}>25 May, Tuesday</Typography>
-            <div style={{marginTop:'20px', display:'flex', flexDirection:'row', alignItems: 'center'}}>
-            {determineIcon("Rainy")}
-            <Typography variant="h6" style={{fontWeight:'bold'}}>Rainy</Typography>
-            </div>
-            <div style={{marginTop:'20px', width:'100%'}}>
-                <Grid container spacing={2}>
-                    <Grid item xs={6} style={{display:'flex', flexDirection:'row', justifyContent:'flex-end', alignItems: 'center'}}>
-                        <WhatshotIcon style={{fontSize:'50px', color:'red'}}/>
-                        <Typography variant="h6" style={{fontWeight:'bold'}}>{temp(props.temperature, isCelsius)}°{isCelsius?"C":"F"}</Typography>
-                    </Grid>
-                    <Grid item xs={6} style={{display:'flex', justifyItems:'flex-start', alignItems:'center'}}>
-                        <FormControlLabel control={<Switch checked={isCelsius} onChange={()=>setIsCelsius(!isCelsius)}/>} label="Fahrenheit / Celsius"/>
-                    </Grid>
+  const classes = useStyles();
+  const isCelsius = props.isCelsius;
+  return (
+    <Paper elevation={2} className={classes.paperstyle}>
+      <Typography variant="h5" style={{ fontWeight: "bold" }}>
+        25 May, Tuesday
+      </Typography>
+      <div
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        {determineIcon(props.status)}
+        <Typography
+          variant="h6"
+          style={{ fontWeight: "bold", marginLeft: "10px" }}
+        >
+          {props.status}
+        </Typography>
+      </div>
+      <Typography variant="body1" style={{ marginTop: "5px" }}>
+        {" "}
+        {props.windspeed} m/s, {BeaufortScale(props.windspeed)}
+      </Typography>
+      <div style={{ marginTop: "20px", width: "100%" }}>
+        <Grid container spacing={3}>
+          <Grid
+            item
+            xs={12}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <div>
+              <Grid container>
+                <Grid
+                  item
+                  xs={8}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "flex-start",
+                    alignItems: "center",
+                  }}
+                >
+                  <WbSunnyIcon style={{ fontSize: "50px", color: "red" }} />
                 </Grid>
-            </div>
-            <div style={{marginTop:'20px', width:'100%'}}>
-                <Grid container spacing={2}>
-                    <Grid item xs={6} style={{display:'flex', flexDirection:'row', justifyContent:'flex-end', alignItems: 'center'}}>
-                        <WhatshotIcon style={{fontSize:'50px', color:'blue'}}/>
-                        <Typography variant="h6" style={{fontWeight:'bold'}}>{temp(props.nightlyTemperature, isNightlyCelsius)}°{isNightlyCelsius?"C":"F"}</Typography>
-                    </Grid>
-                    <Grid item xs={6} style={{display:'flex', justifyItems:'flex-start', alignItems:'center'}}>
-                        <FormControlLabel control={<Switch style={{color:'blue'}} checked={isNightlyCelsius} onChange={()=>setIsNightlyCelsius(!isNightlyCelsius)}/>} label="Fahrenheit / Celsius"/>
-                    </Grid>
+                <Grid
+                  item
+                  xs={4}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "flex-end",
+                  }}
+                >
+                  <Typography
+                    variant="body1"
+                    style={{ fontWeight: "bold", marginLeft: "10px" }}
+                  >
+                    {temp(props.temperature, isCelsius)}°{isCelsius ? "C" : "F"}
+                  </Typography>
                 </Grid>
+              </Grid>
+              <Grid container style={{ marginTop: "20px" }}>
+                <Grid
+                  item
+                  xs={6}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "flex-start",
+                    alignItems: "center",
+                  }}
+                >
+                  <Brightness3Icon
+                    style={{ fontSize: "50px", color: "blue" }}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  xs={6}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "flex-end",
+                  }}
+                >
+                  {" "}
+                  <Typography
+                    variant="body1"
+                    style={{ fontWeight: "bold", marginLeft: "10px" }}
+                  >
+                    {temp(props.nightlyTemperature, isCelsius)}°
+                    {isCelsius ? "C" : "F"}
+                  </Typography>
+                </Grid>
+              </Grid>
             </div>
-        </Paper>
-    );
+          </Grid>
+        </Grid>
+      </div>
+      <div
+        style={{
+          marginTop: "20px",
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <InvertColorsIcon color="primary" style={{ fontSize: "50px" }} />
+        <Typography variant="h6" style={{ fontWeight: "bold" }}>
+          Humidity: {props.humidity}%
+        </Typography>
+      </div>
+      <div
+        style={{
+          marginTop: "20px",
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          width: "75%",
+        }}
+      >
+        <WbSunnyIcon style={{ color: "orangered", fontSize: "50px" }} />
+        <Typography variant="h6" style={{ fontWeight: "bold" }}>
+          Sunrise: {props.sunrise}
+        </Typography>
+        <Brightness4OutlinedIcon style={{ color: "blue", fontSize: "50px" }} />
+        <Typography variant="h6" style={{ fontWeight: "bold" }}>
+          Sunset: {props.sunset}
+        </Typography>
+      </div>
+    </Paper>
+  );
 }
 
 export default DayItem;
--- client-app/src/pages/WeatherDisplay.js
+++ client-app/src/pages/WeatherDisplay.js
@@ -1,67 +1,234 @@
-import { Container, makeStyles, Typography, Select, Button, MenuItem } from '@material-ui/core';
-import React, { useState } from 'react';
-import theme from '../theme';
-import RoomIcon from '@material-ui/icons/Room';
-import WatchLaterIcon from '@material-ui/icons/WatchLater';
-import DayItem from '../components/DayItem';
+import {
+  Container,
+  makeStyles,
+  Typography,
+  Select,
+  Button,
+  MenuItem,
+  Grid,
+  FormControlLabel,
+  Switch,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import theme from "../theme";
+import RoomIcon from "@material-ui/icons/Room";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import DayItem from "../components/DayItem";
 
 const useStyles = makeStyles({
-    background: {
-        backgroundColor: '#D3D3D3',
-        height:'100vh',
-        width: '100vw'
-    },
-    container:{
-        paddingTop:'20px'
-    },
-    optionmenu:
-    {
-        display:'flex',
-        flexDirection:'row',
-        paddingTop:'30px'
-    }
+  background: {
+    backgroundColor: "#D3D3D3",
+  },
+  container: {
+    paddingTop: "20px",
+    minHeight: "100vh",
+  },
+  optionmenu: {
+    display: "flex",
+    flexDirection: "row",
+    paddingTop: "30px",
+    alignItems: "center",
+    paddingBottom: "20px",
+    justifyContent: "space-evenly",
+  },
 });
 
+function DailyReport(props) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+    >
+      <DayItem
+        status="Cloudy"
+        temperature={20}
+        nightlyTemperature={7}
+        humidity={6}
+        windspeed={40}
+        sunrise="06:27"
+        sunset="22:07"
+        isCelsius={props.isCelsius}
+      />
+    </div>
+  );
+}
+
+function WeeklyReport(props) {
+  return (
+    <>
+      <Grid
+        container
+        spacing={2}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <Grid item xs={4}>
+          <DayItem
+            status="Cloudy"
+            temperature={20}
+            nightlyTemperature={7}
+            humidity={6}
+            windspeed={40}
+            sunrise="06:27"
+            sunset="22:07"
+            isCelsius={props.isCelsius}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <DayItem
+            status="Cloudy"
+            temperature={20}
+            nightlyTemperature={7}
+            humidity={6}
+            windspeed={40}
+            sunrise="06:27"
+            sunset="22:07"
+            isCelsius={props.isCelsius}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <DayItem
+            status="Cloudy"
+            temperature={20}
+            nightlyTemperature={7}
+            humidity={6}
+            windspeed={40}
+            sunrise="06:27"
+            sunset="22:07"
+            isCelsius={props.isCelsius}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <DayItem
+            status="Cloudy"
+            temperature={20}
+            nightlyTemperature={7}
+            humidity={6}
+            windspeed={40}
+            sunrise="06:27"
+            sunset="22:07"
+            isCelsius={props.isCelsius}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <DayItem
+            status="Cloudy"
+            temperature={20}
+            nightlyTemperature={7}
+            humidity={6}
+            windspeed={40}
+            sunrise="06:27"
+            sunset="22:07"
+            isCelsius={props.isCelsius}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <DayItem
+            status="Cloudy"
+            temperature={20}
+            nightlyTemperature={7}
+            humidity={6}
+            windspeed={40}
+            sunrise="06:27"
+            sunset="22:07"
+            isCelsius={props.isCelsius}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <DayItem
+            status="Cloudy"
+            temperature={20}
+            nightlyTemperature={7}
+            humidity={6}
+            windspeed={40}
+            sunrise="06:27"
+            sunset="22:07"
+            isCelsius={props.isCelsius}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+function ForecastDisplay(props) {
+  if (props.interval === 1 || props.interval === 2) {
+    return <DailyReport isCelsius={props.isCelsius} />;
+  } else if (props.interval === 3) {
+    return <WeeklyReport isCelsius={props.isCelsius} />;
+  } else {
+    return <></>;
+  }
+}
+
 function WeatherDisplay() {
-    const classes = useStyles();
-    const [city, setCity] = useState(0);
-    const [interval, setInterval] = useState(0);
-    const onIntervalChange = (event) => {
-        setInterval(event.target.value);
-    };
-    const onCityChange = (event) => {
-        setCity(event.target.value);
-    };
-    return (
-        <>
-        <div className={classes.background}>
-            <Container className={classes.container}>
-                <Typography variant="h3" color="primary">
-                    WEATHER FORECAST APP
-                </Typography>
-                <div className={classes.optionmenu}>
-                    <RoomIcon color="primary"/>
-                    <Typography variant="body1" color="primary">Select city: </Typography>
-                    <Select value={city} onChange={onCityChange} style={{marginRight:'20px', marginLeft:'20px'}}>
-                        <MenuItem value={0}>Not Selected</MenuItem>
-                    </Select>
-                    <WatchLaterIcon color="primary"/>
-                    <Typography variant="body1" color="primary">Select time interval: </Typography>
-                    <Select value={interval} onChange={onIntervalChange} color="primary" style={{marginLeft:'20px',marginRight:'20px'}}>
-                        <MenuItem value={0}>Not Selected</MenuItem>
-                        <MenuItem value={1}>Today</MenuItem>
-                        <MenuItem value={2}>Tommorrow</MenuItem>
-                        <MenuItem value={3}>Weekly</MenuItem>
-                    </Select>
-                    <Button color="primary" variant="contained">FETCH DATA</Button>
-                </div>
-                <div style={{margin:'20px'}}>
-                <DayItem temperature={20} nightlyTemperature={7}/>
-                </div>
-            </Container>
-        </div>
-        </>
-    );
+  const classes = useStyles();
+  const [city, setCity] = useState(0);
+  const [interval, setInterval] = useState(1);
+  const [isCelsius, setIsCelsius] = useState(true);
+  const onIntervalChange = (event) => {
+    setInterval(event.target.value);
+  };
+  const onCityChange = (event) => {
+    setCity(event.target.value);
+  };
+  return (
+    <>
+      <div className={classes.background}>
+        <Container className={classes.container}>
+          <Typography variant="h3" color="primary">
+            WEATHER FORECAST APP
+          </Typography>
+          <div className={classes.optionmenu}>
+            <RoomIcon color="primary" />
+            <Typography variant="body1" color="primary">
+              Select city:{" "}
+            </Typography>
+            <Select
+              value={city}
+              onChange={onCityChange}
+              style={{ marginRight: "20px", marginLeft: "20px" }}
+            >
+              <MenuItem value={0}>Not Selected</MenuItem>
+            </Select>
+            <WatchLaterIcon color="primary" />
+            <Typography variant="body1" color="primary">
+              Select time interval:{" "}
+            </Typography>
+            <Select
+              value={interval}
+              onChange={onIntervalChange}
+              color="primary"
+              style={{ marginLeft: "20px", marginRight: "20px" }}
+            >
+              <MenuItem value={1}>Today</MenuItem>
+              <MenuItem value={2}>Tommorrow</MenuItem>
+              <MenuItem value={3}>Weekly</MenuItem>
+            </Select>
+            <Button color="primary" variant="contained">
+              FETCH DATA
+            </Button>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={isCelsius}
+                  onChange={() => setIsCelsius(!isCelsius)}
+                />
+              }
+              label="Fahrenheit / Celsius"
+            />
+          </div>
+          <ForecastDisplay interval={interval} isCelsius={isCelsius} />
+        </Container>
+      </div>
+    </>
+  );
 }
 
 export default WeatherDisplay;
